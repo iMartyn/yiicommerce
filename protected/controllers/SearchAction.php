794179588b6c4ef448f2e801322b591ff42c27aa
iMartyn <ranyardm@gmail.com>
2012-03-22 23:47:48 +0000

    Messing around with search controller as an example of what to do

---
+++ protected/controllers/SearchAction.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * SearchAction class file.
+ *
+ * @author Jon Doe <jonny@gmail.com>
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright &copy; 2008-2011 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+/**
+ * SearchAction is ...
+ *
+ *
+ * @author Jon Doe <jonny@gmail.com>
+ * @version
+ * @package
+ * @since 1.0
+ */
+
+
+class SearchAction extends CAction
+{
+
+    public function run()
+    {
+        // place the action logic here
+        echo "Wooooooo!"
+    }
+    
+}
